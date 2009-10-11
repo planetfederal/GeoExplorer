c44e0feb294c800b1b9405b5c545472edce28ceb
Tim Schaub <tim.schaub@gmail.com>
2009-10-11 04:42:43 +0000

    Use the layer menu from gxp.
    git-svn-id: http://svn.opengeo.org/suite/trunk/geoexplorer@329 fe66add1-021f-4af3-b75b-ea5154e73f91

--- src/client/script/app/GeoExplorer/Embed.js
+++ src/client/script/app/GeoExplorer/Embed.js
@@ -48,51 +48,12 @@ GeoExplorer.Embed = Ext.extend(GeoExplorer, {
     createTools: function() {
         var tools = GeoExplorer.Embed.superclass.createTools.apply(this, arguments);
 
-        var menu = new Ext.menu.Menu();
-
-        var updateLayerSwitcher = function() {
-            menu.removeAll();
-            menu.getEl().addClass("gx-layer-menu");
-            menu.getEl().applyStyles({
-                width: '',
-                height: ''
-            });
-            menu.add(
-                {
-                    iconCls: "gx-layer-visibility",
-                    text: "Layer",
-                    canActivate: false
-                },
-                "-"
-            );
-            this.mapPanel.layers.each(function(record) {
-                var layer = record.get("layer");
-                if(layer.displayInLayerSwitcher) {
-                    var item = new Ext.menu.CheckItem({
-                        text: record.get("title"),
-                        checked: record.get("layer").getVisibility(),
-                        group: record.get("group"),
-                        listeners: {
-                            checkchange: function(item, checked) {
-                                record.get("layer").setVisibility(checked);
-                            }
-                        }
-                    });
-                    if (menu.items.getCount() > 2) {
-                        menu.insert(2, item);
-                    } else {
-                        menu.add(item);
-                    }
-                }
-            });
-        };
-
-        this.mapPanel.layers.on("add", updateLayerSwitcher, this);
-
         var layerChooser = new Ext.Button({
             tooltip: 'Layer Switcher',
             iconCls: 'icon-layer-switcher',
-            menu: menu
+            menu: new gxp.menu.LayerMenu({
+                layers: this.mapPanel.layers
+            })
         });
 
         tools.unshift("-");
