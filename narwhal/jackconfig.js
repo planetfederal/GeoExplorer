0abb5da6d6801a3eed1014606d44d3f49b0f3952
Tim Schaub <tim.schaub@gmail.com>
2010-03-12 05:16:16 +0000

    Working development app (without persistence).
    git-svn-id: http://svn.opengeo.org/suite/trunk/geoexplorer@887 fe66add1-021f-4af3-b75b-ea5154e73f91

--- narwhal/jackconfig.js
+++ narwhal/jackconfig.js
@@ -7,20 +7,8 @@ exports.development = function(app) {
     return require("jack/contentlength").ContentLength(
         URLMap({
             "/": app,
-            "/geoexplorer/script/": require("autoloader").App("../../build/jsbuild.cfg")
+            "/geoexplorer/script/": require("autoloader").App("../build/jsbuild.cfg")
         })
     );
 
 };
-
-//var Request = require("jack/request").Request;
-//var HttpClient = require("http-client").HttpClient;
-//
-//exports.app = function(env) {
-//    
-//    var client = new HttpClient({
-//        url: "http://demo.opengeo.org/geoserver/wms?request=GetCapabilities"
-//    });
-//    return HttpClient.decode(client.connect());    
-//    
-//};
