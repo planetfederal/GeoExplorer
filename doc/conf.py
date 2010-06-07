e4ef1ec71bcf2b262f85fdd7c7dcaf43aa1ab63c
Mike Pumphrey <USER@DOMAIN>
2010-06-07 19:34:16 +0000

    Version change to 1.9.2 for everything except for Mac artifacts
    git-svn-id: http://svn.opengeo.org/suite/trunk/geoexplorer@1508 fe66add1-021f-4af3-b75b-ea5154e73f91

--- doc/conf.py
+++ doc/conf.py
@@ -44,9 +44,9 @@ copyright = u'2009 OpenGeo'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.2'
+version = '1.9.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.2'
+release = '1.9.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
