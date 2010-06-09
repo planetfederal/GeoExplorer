5335864670e68a0c819f2b6755a5b051a9d7b24e
Mike Pumphrey <USER@DOMAIN>
2010-06-09 15:11:00 +0000

    Setting version numbers from 1.9.2 to 2.0.0
    git-svn-id: http://svn.opengeo.org/suite/trunk/geoexplorer@1555 fe66add1-021f-4af3-b75b-ea5154e73f91

--- doc/conf.py
+++ doc/conf.py
@@ -44,9 +44,9 @@ copyright = u'2009 OpenGeo'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '1.9.2'
+version = '2.0.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.9.2'
+release = '2.0.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
