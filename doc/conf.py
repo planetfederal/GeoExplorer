67dc4cd8c6b0cfdba0fe14a2a695a92434694ac0
Tim Schaub <tim.schaub@gmail.com>
2010-07-01 20:00:56 +0000

    Changing 40 copies of the Suite version number.
    git-svn-id: http://svn.opengeo.org/suite/trunk/geoexplorer@1627 fe66add1-021f-4af3-b75b-ea5154e73f91

--- doc/conf.py
+++ doc/conf.py
@@ -44,9 +44,9 @@ copyright = u'2009 OpenGeo'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.0.0'
+version = '2.0.1'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0'
+release = '2.0.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
