c43d1d668e286bc4d3efc6ed14f14750d1966e70
Tim Schaub <tim.schaub@gmail.com>
2010-07-15 04:34:00 +0000

    GeoExplorer back-end updates to support printing (see #661).
    git-svn-id: http://svn.opengeo.org/suite/trunk/geoexplorer@1642 fe66add1-021f-4af3-b75b-ea5154e73f91

--- package/lib/proxy.js
+++ package/lib/proxy.js
@@ -3,25 +3,46 @@ var Request = require("jack").Request;
 var responseForStatus = require("jack/utils").responseForStatus;
 
 var app = function(env) {
+
+    var request = new Request(env);
     
     var resp;
-    var request = new Request(env);
     var url = request.GET("url");
     if (url) {
         var client = new HttpClient({
             url: url,
             method: env["REQUEST_METHOD"]
-            // TODO: headers
         });
-        var body = request.body();
-        if (body && body.forEach) {
+        var contentType = request.contentType();
+        if (contentType) {
+            client.setHeader("Content-Type", contentType);
+        }
+        var contentLength = request.contentLength();
+        if (contentLength) {
+            client.setHeader("Content-Length", contentLength);
+        }
+        // TODO: remaining headers
+        var body;
+        var input = env["jsgi.input"];
+        if (input) {
+            var charSet = request.contentCharset();
+            body = {
+                forEach: function(callback) {
+                    callback(input.readChunk().toString(charSet));
+                }
+            };
+        }
+        if (body) {
+            if (!contentLength) {
+                throw new Error("Can't proxy body without Content-Length header");
+            }
             client.setOption("body", body);
         }
         resp = HttpClient.decode(client.connect());
     } else {
         resp = responseForStatus(400);
     }
-    
+        
     return resp;
 
 };
