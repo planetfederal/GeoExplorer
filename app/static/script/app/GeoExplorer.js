9251ba9c2a9db27de8771f644905cfae5dae347f
Fran Boon <francisboon@gmail.com>
2011-01-23 14:34:51 +0000

    Remove annoying header from westPanel, remove workarounds for older versions of OpenLayers & GeoExt

--- app/static/script/app/GeoExplorer.js
+++ app/static/script/app/GeoExplorer.js
@@ -3,22 +3,6 @@
  */
 
 /**
- * Patch the SphericalMercator layer to respect projection from configuration.
- * TODO: remove this when http://trac.openlayers.org/ticket/2665 is closed
- */
-(function() {
-    var proto = OpenLayers.Layer.SphericalMercator;
-    var original = proto.initMercatorParameters;
-    proto.initMercatorParameters = function() {
-        original.apply(this, arguments);
-        // respect configured projection code
-        if (this.options && this.options.projection) {
-            this.projection = this.options.projection;
-        }
-    };
-})();
- 
-/**
  * Add transforms for EPSG:102113.  This is web mercator to ArcGIS 9.3.
  */
 OpenLayers.Projection.addTransform(
@@ -495,7 +479,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
             map: this.mapPanel.map,
             defaults: {cls: 'legend-item'}
             // TODO: remove when http://trac.geoext.org/ticket/305 is fixed
-            ,items: []
+            //,items: []
         });        
 
         var westPanel = new Ext.Panel({
@@ -506,6 +490,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
             split: true,
             collapsible: true,
             collapseMode: "mini",
+            header: false,
             items: [
                 layersContainer, legendContainer
             ]
