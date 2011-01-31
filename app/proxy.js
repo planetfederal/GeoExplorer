ce73b832287267abcc071f7807fc01ddb853bab5
tschaub <tim.schaub@gmail.com>
2011-01-31 16:23:42 -0700

    Update proxy to support simple pass.

--- app/proxy.js
+++ app/proxy.js
@@ -1,39 +1,58 @@
 var Client = require("ringo/httpclient").Client;
-var {Request} = require("ringo/webapp/request");
-var {MemoryStream} = require("io");
-var {merge} = require("ringo/utils/objects");
+var Request = require("ringo/webapp/request").Request;
+var MemoryStream = require("io").MemoryStream;
+var merge = require("ringo/utils/objects").merge;
 var responseForStatus = require("./util").responseForStatus;
+var defer = require("ringo/promise").defer;
 
-var app = function(env) {
-    var resp;
+var app = exports.app = function(env) {
+    var response;
     var request = new Request(env);
     var url = request.queryParams.url;
     if (url) {
-        var parts = url.split("/");
-        if (parts[0] !== (request.scheme + ":") || parts[1] !== "") {
-            resp = responseForStatus(400, "The url parameter value must be absolute url with same scheme as request.");
-        } else {
-            // re-issue request
-            var host = parts[2];
-            var client = new Client();
-            var exchange = client.request({
-                url: url,
-                method: request.method,
-                headers: merge({host: host}, request.headers),
-                data: request.contentLength && request.input
-            });
-            exchange.wait();
-
-            resp = {
-                status: exchange.status,
-                headers: exchange.headers,
-                body: new MemoryStream(exchange.contentBytes)
-            };
-        }
+        response = proxyPass(request, url);
     } else {
-        resp = responseForStatus(400, "Request must contain url parameter.");
+        response = responseForStatus(400, "Request must contain url parameter.");
     }
-    return resp;
+    return response;
 };
 
-exports.app = app;
+var pass = exports.pass = function(config) {
+    if (typeof config == "string") {
+        config = {url: config};
+    }
+    return function(env, match) {
+        var request = new Request(env);
+        var newUrl = config.url + match + (request.queryString ? "?" + request.queryString : "");
+        return proxyPass(request, newUrl, config.preserveHost);
+    };
+};
+
+function proxyPass(request, url, preserveHost) {
+    var parts = url.split("/");
+    var response;
+    if (parts[0] !== (request.scheme + ":") || parts[1] !== "") {
+        print(parts);
+        response = responseForStatus(400, "The url parameter value must be absolute url with same scheme as request.");
+    } else {
+        // re-issue request
+        var host = parts[2];
+        var client = new Client();
+        response = defer();
+        var exchange = client.request({
+            url: url,
+            method: request.method,
+            headers: preserveHost ? merge({host: host}, request.headers) : request.headers,
+            data: request.contentLength && request.input,
+            async: true,
+            complete: function() {
+                response.resolve({
+                    status: exchange.status,
+                    headers: exchange.headers,
+                    body: new MemoryStream(exchange.contentBytes)
+                });
+            }
+        });
+    }
+    return response;
+}
