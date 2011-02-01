0ce0aabce4a5083e1e84b292bf1acd15eff68064
Юджин <lazareveugene@eugene-laptop.(none)>
2011-02-01 23:13:31 +0500

    added ru.js file

---
+++ app/static/script/app/locale/ru.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2009-2010 The Open Planning Project
+ */
+
+GeoExt.Lang.add("ru", {
+    "GeoExplorer.prototype": {
+        zoomSliderText: "<div>Уровень: {zoom}</div><div>Масштаб: 1:{scale}</div>",
+        loadConfigErrorText: "Ошибка чтения сохраненной конфигурации: <br />",
+        loadConfigErrorDefaultText: "Ошибка сервера.",
+        xhrTroubleText: "Ошибка связи: Статус ",
+        layersText: "Слои",
+        titleText: "Заголовок",
+        zoomLevelText: "Уровень",
+        switch3dText: "3D Вид",
+        previewText: "Предпросмотр печати",
+        printText: "На печать",
+        notAllNotPrintableText: "Не все слои могут быть напечатаны", 
+        nonePrintableText: "Ни один слой не может быть напечатан",
+        saveErrorText: "Ошибка сохранения: ",
+        bookmarkText: "URL закладки",
+        permakinkText: 'Постоянная ссылка',
+        appInfoText: "GeoExplorer",
+        aboutText: "О GeoExplorer",
+        mapInfoText: "Описание карты",
+        descriptionText: "Описание",
+        contactText: "Контакты",
+        aboutThisMapText: "Об этой карте"
+    },
+    "GeoExplorer.Composer.prototype": {
+        publishMapText: "Опубликовать карту",
+        saveMapText: "Сохранить карту"
+    }
+});
