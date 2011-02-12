ddc657a129f06006dbbd03f012580ee6a35b8d50
tschaub <tim.schaub@gmail.com>
2011-02-12 16:01:05 -0700

    Hitting j_spring_security_check for auth.

--- app/auth.js
+++ app/auth.js
@@ -1,10 +1,22 @@
 var Client = require("ringo/httpclient").Client;
 
 function getAuthUrl(request) {
-    return request.scheme + "://" + request.host + (request.port ? ":" + request.port : "") + "/geoserver/rest";
+    var url = java.lang.System.getProperty("app.proxy.geoserver");
+    if (url) {
+        if (url.charAt(url.length-1) !== "/") {
+            url = url + "/";
+        }
+    } else {
+        url = request.scheme + "://" + request.host + (request.port ? ":" + request.port : "") + "/geoserver/";
+    }
+    return url + "j_spring_security_check";
 }
 
 var checkStatus = exports.checkStatus = function(request) {
+    return requestAuthorization(request).status;
+};
+
+var requestAuthorization = exports.requestAuthorization = function(request) {
     var url = getAuthUrl(request);
     var client = new Client();
     var exchange = client.request({
@@ -14,6 +26,5 @@ var checkStatus = exports.checkStatus = function(request) {
         async: false
     });
     exchange.wait();
-    return exchange.status;
+    return exchange;
 };
-
