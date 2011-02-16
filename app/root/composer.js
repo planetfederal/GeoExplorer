a3758cd55e6e1510356d4e0bd7e4e6840ad4f15b
tschaub <tim.schaub@gmail.com>
2011-02-16 12:25:50 -0700

    Redirecting to index if not authorized for composer.

--- app/root/composer.js
+++ app/root/composer.js
@@ -1,5 +1,22 @@
 var Response = require("ringo/webapp/response").Response;
+var Request = require("ringo/webapp/request").Request;
+var auth = require("../auth");
 
-exports.app = function(app) {
-    return Response.skin(module.resolve("../skins/composer.html"));
+exports.app = function(req) {
+    var request = new Request(req);
+    var details = auth.getDetails(request);
+    var response;
+    if (details.status === 401) {
+        var parts = request.path.split("/");
+        parts.pop();
+        parts.push("");
+        response = {
+            status: 301,
+            headers: {"Location": request.scheme + "://" + request.host + ":" + request.port + parts.join("/")},
+            body: []
+        };
+    } else {
+        response = Response.skin(module.resolve("../skins/composer.html"));
+    }
+    return response;
 };
