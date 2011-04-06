3202192e15921044059480193da8c829cef4c194
tschaub <tim.schaub@gmail.com>
2011-04-05 18:04:33 -0600

    Use request method instead of Client constructor.

--- app/proxywms.js
+++ app/proxywms.js
@@ -1,21 +1,20 @@
-    var Client = require("ringo/httpclient").Client;
-    var Request = require("ringo/webapp/request").Request;
-    var MemoryStream = require("io").MemoryStream;
+var clientRequest = require("ringo/httpclient").request;
+var Request = require("ringo/webapp/request").Request;
+var MemoryStream = require("io").MemoryStream;
 
-    var app = exports.app = function(env) {
-        var response;
-        var request = new Request(env);
-        var client = new Client();
-        var exchange = client.request({
-            url: "http://localhost:8080/geoserver/wms?" + request.queryString,
-            method: "GET",
-            async: false
-        });
-        exchange.wait();
-        return {
-            status: exchange.status,
-            headers: exchange.headers,
-            body: new MemoryStream(exchange.contentBytes)
-        };
+var app = exports.app = function(env) {
+    var response;
+    var request = new Request(env);
+    var exchange = clientRequest({
+        url: "http://localhost:8080/geoserver/wms?" + request.queryString,
+        method: "GET",
+        async: false
+    });
+    exchange.wait();
+    return {
+        status: exchange.status,
+        headers: exchange.headers,
+        body: new MemoryStream(exchange.contentBytes)
     };
+};
 
