46179c0df9029688072adf5c7309c07f0409f720
Tim Schaub <tim.schaub@gmail.com>
2011-04-12 19:15:30 -0600

    Removing rejected index page.

--- app/root/index.js
+++ app/root/index.js
@@ -1,14 +1,5 @@
 var Request = require("ringo/webapp/request").Request;
 var Response = require("ringo/webapp/response").Response;
-//var auth = require("../auth");
-
-var nixed = function(env) {
-    var request = new Request(env);
-    var details = auth.getDetails(request);
-    return Response.skin(module.resolve("../skins/index.html"), {
-        status: details.status
-    });
-};
 
 exports.app = function(env) {
     var request = new Request(env);
