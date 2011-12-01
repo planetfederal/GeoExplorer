57f45cfef8e333030b2b509118cd9c1381b7836b
Tim Schaub <tim.schaub@gmail.com>
2011-11-30 16:24:10 -0800

    Simplify autoloader based on buildkit changes.
    Buildkit now returns an ordered list of data objects representing scripts for each configured section.  These data objects include the root for each script path.  The autoloader now produces a lib loader that includes the relative path to the script.  This path can be used to configure static middleware.

--- app/autoloader.js
+++ app/autoloader.js
@@ -9,9 +9,8 @@ var MERGE = require("buildkit").merge;
 var template = getResource("./templates/debug-loader.js").getContent();
 
 var libLoader = function(section, order) {
-    var paths = [];
-    order.forEach(function(path) {
-        paths.push("'@" + section + "/" + path + "'");
+    var paths = order.map(function(script) {
+        return "'@" + "/" + script.root + "/" + script.path + "'";
     });
     var body = template.replace("{{paths}}", paths.join(",\n"));
     return function(env) {
@@ -23,92 +22,25 @@ var libLoader = function(section, order) {
     };
 };
 
-var scriptLoader = function(root, script) {
-    return function(env) {
-        var path = FS.join(root, script);
-        var body = FS.read(path);
-        return {
-            status : 200,
-            headers : {
-                "Last-Modified": FS.lastModified(path).toUTCString(),
-                "Content-Type": "text/javascript",
-                "Content-Length": body.length.toString(10)
-            },
-            body : [body]
-        };
-    };
-};
-
 var App = function(config) {
     var sections = CONFIG.parse(config);
-    var group, root, order, app, urls = {};
+    var group, root, order;
+    var app = Application();
+    app.configure("mount");
     for (var section in sections) {
         group = sections[section];
-        // make root relative to config
-        root = FS.join(FS.directory(config), group.root[0]);
-        group.root = [root];
         order = MERGE.order(group);
-        // create lib loader
-        urls["/" + section] = libLoader(section, order);
-        // create static loader for all scripts in lib
-        app = Application();
-        app.configure("static");
-        app.static(root);
-        urls["/@" + section] = app;
+        // mount lib loader for the section
+        app.mount("/" + section, libLoader(section, order));
     }
-    return URLMap(urls);
-    
+    // mount a script loader for all scripts
+    var scriptLoader = Application();
+    scriptLoader.configure("static");
+    print(FS.directory(config));
+    scriptLoader.static(FS.directory(config));
+    app.mount("/@", scriptLoader);
+    return app;
 };
 
 exports.App = App;
 
-var URLMap = function(map, options) {
-    var options = options || { longestMatchFirst : true },
-        mapping = [];
-        
-    for (var location in map) {
-        var app = map[location],
-            host = null,
-            match;
-        
-        if (match = location.match(/^https?:\/\/(.*?)(\/.*)/)) {
-            host = match[1];
-            location = match[2];
-        }
-            
-        if (location.charAt(0) != "/") {
-            throw new Error("paths need to start with / (was: " + location + ")");
-        }
-        
-        mapping.push([host, location.replace(/\/+$/,""), app]);
-    }
-    // if we want to match longest matches first, then sort
-    if (options.longestMatchFirst) {
-        mapping = mapping.sort(function(a, b) {
-            return (b[1].length - a[1].length) || ((b[0]||"").length - (a[0]||"").length);
-        });
-    }
-    
-    return function(env) {
-        var path = env.pathInfo.replace(/\/+$/,"");
-        var hHost = env.host, sPort = env.port;
-
-        for (var i = 0; i < mapping.length; i++) {
-            var host = mapping[i][0], location = mapping[i][1], app = mapping[i][2];
-
-            if ((host === hHost || (host === null)) &&
-                (location === path.substring(0, location.length)) &&
-                (path.charAt(location.length) === "" || path.charAt(location.length) === "/"))
-            {
-                env = Object.create(env); // make a shallow "copy", since we're modifying SCRIPT_NAME / PATH_INFO
-
-                env.scriptName += location;
-                env.pathInfo = path.substring(location.length);
-
-                return app(env);
-            }
-        }
-
-        throw {notfound: true};
-    }
-}
