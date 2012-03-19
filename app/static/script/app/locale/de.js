12e53e448f5386f1e1b08eaa5c323d7656e30704
ahocevar <andreas.hocevar@gmail.com>
2012-03-19 17:49:43 +0100

    Adding German translation

---
+++ app/static/script/app/locale/de.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2009-2010 The Open Planning Project
+ */
+
+GeoExt.Lang.add("de", {
+    "GeoExplorer.prototype": {
+        zoomSliderText: "<div>Zoomstufe: {zoom}</div><div>Maßstab: 1:{scale}</div>",
+        loadConfigErrorText: "Gespeicherte Konfiguration kann nicht gelesen werden: <br />",
+        loadConfigErrorDefaultText: "Server Fehler.",
+        xhrTroubleText: "Kommunikationsfehler: Status ",
+        layersText: "Layer",
+        titleText: "Titel",
+        zoomLevelText: "Zoomstufe",
+        bookmarkText: "Bookmark URL",
+        permakinkText: 'Permalink',
+        appInfoText: "GeoExplorer",
+        aboutText: "Über GeoExplorer",
+        mapInfoText: "Karteninfo",
+        descriptionText: "Beschreibung",
+        contactText: "Kontakt",
+        aboutThisMapText: "Über diese Karte"
+    },
+    "GeoExplorer.Composer.prototype": {
+        exportMapText: "Karte veröffentlichen",
+        saveMapText: "Karte speichern",
+        saveErrorText: "Beim Speichern ist ein Problem aufgetreten: ",
+        toolsTitle: "Werkzeuge, die in der Werkzeugleiste zur Verfügung stehen sollen:",
+        previewText: "Vorschau",
+        backText: "Zurück",
+        nextText: "Weiter",
+        loginText: "Login",
+        loginErrorText: "Ungültiger Benutzername oder ungültiges Kennwort.",
+        userFieldText: "Benutzer",
+        passwordFieldText: "Kennwort"
+    }
+});
