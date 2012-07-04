17723e7bb21f18f4abddc0dad42a1f83195ea43f
tschaub <tim.schaub@gmail.com>
2012-07-03 18:06:38 -0600

    Convenient links for GeoServer (fixes SUITE-530).
    The layers query string parameter adds overlays from the local layer source.  The bbox query string parameter sets the initial map extent (in the map CRS).

--- app/static/script/app/GeoExplorer.js
+++ app/static/script/app/GeoExplorer.js
@@ -227,10 +227,38 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
             });
         } else {
             var query = Ext.urlDecode(document.location.search.substr(1));
-            if (query && query.q) {
-                var queryConfig = Ext.util.JSON.decode(query.q);
-                Ext.apply(config, queryConfig);
+            if (query) {
+                if (query.q) {
+                    var queryConfig = Ext.util.JSON.decode(query.q);
+                    Ext.apply(config, queryConfig);
+                }
+                /**
+                 * Special handling for links from local GeoServer.
+                 *
+                 * The layers query string value indicates layers to add as 
+                 * overlays from the local source.
+                 *
+                 * The bbox query string value indicates the initial extent in
+                 * the current map projection.
+                 */
+                 if (query.layers) {
+                     var layers = query.layers.split(/\s*,\s*/);
+                     for (var i=0,ii=layers.length; i<ii; ++i) {
+                         config.map.layers.push({
+                             source: "local",
+                             name: layers[i],
+                             title: layers[i],
+                             visibility: true
+                         });
+                     }
+                 }
+                 if (query.bbox) {
+                     delete config.map.zoom;
+                     delete config.map.center;
+                     config.map.extent = query.bbox.split(/\s*,\s*/);
+                 }
             }
+            
             this.applyConfig(config);
         }
         
